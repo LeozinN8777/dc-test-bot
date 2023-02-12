7529616ce278199b8ac2fcce1c62e039b2c291ba
Leonardo de Martini <demartinileo@hotmail.com>
2023-02-12 14:12:13 -0300

    VIP completo

--- src/events/ready/checkVIP.js
+++ src/events/ready/checkVIP.js
@@ -1,57 +1,60 @@
 const discord = require("discord.js");
 const vipsDB = require("../../database/vip");
 
-module.exports = (client, guild) => {
-    const logChannel = client.channels.cache.get('1042145058092240969');
+module.exports = (client) => {
+  const guild = client.guilds.cache.get('976460350574719016')
+  const logChannel = client.channels.cache.get("1042145058092240969");
 
-    // canal de log
+  // canal de log
 
-    setInterval( async () => {
-        const vipsDBs = await vipsDB.find();
+  setInterval(async () => {
+    const vipsDBs = await vipsDB.find();
 
-        for (let i = 0; i < vipsDBs.length; i++) {
-            async function removeVIP() {
-                await vipsDB
-                .deleteOne({ userID: vipsDBs[i].userID })
-                .then(() => {
-                //   let member = guild.members.getMember(vipsDBs[i].userID);
-                //   member.roles.remove(vipsDBs[i].VIPRoleID)
-                  logChannel.send({embeds: {content: `VIP REMOVIDO!`, embeds: [embed]}});
-                })
-                .catch((error) => {
-                  logChannel.send({
-                    content: `@here, ocorreu um problema ao tentar remover o VIP: ${error}`,
-                  });
-                });
-            }
+    for (let i = 0; i < vipsDBs.length; i++) {
+      async function removeVIP() {
+        await vipsDB
+          .deleteOne({ userID: vipsDBs[i].userID })
+          .then(() => {
+            const member = guild.members.cache.get(vipsDBs[i].userID);
+            member.roles.remove(vipsDBs[i].VIPRoleID) 
+            console.log(`VIP REMOVIDO - ${vipsDBs[i].userTAG} - ${vipsDBs[i].userID}`)
+          })
+          .catch((error) => {
+            logChannel.send({
+              content: `@here, ocorreu um problema ao tentar remover o VIP: ${error}`,
+            });
+          });
+      }
 
-            let dateNow = await new Date()
-            
-            if (dateNow > vipsDBs[i].Fim){
-                let embed = new discord.EmbedBuilder()
-                .setColor('Red')
-                .setTitle(`${vipsDBs[i].userTAG} - ${vipsDBs[i].VIP}`)
-                .setDescription(`userID: ${vipsDBs[i].userID} \n VIPRoleID: ${vipsDBs[i].VIPRoleID}`)
-                .addFields(
-                    { name: `Início:`, value: `${vipsDBs[i].Inicio}`},
-                    { name: `Fim:`, value: `${vipsDBs[i].Fim}`},
-                )
+      let dateNow = await new Date();
 
-                await removeVIP()
+      if (dateNow > vipsDBs[i].Fim) {
+        let embed = new discord.EmbedBuilder()
+        .setColor("Red")
+        .setTitle(`${vipsDBs[i].userTAG} - ${vipsDBs[i].VIP}`)
+        .setDescription(
+          `userID: ${vipsDBs[i].userID} \n VIPRoleID: ${vipsDBs[i].VIPRoleID}`
+        )
+        .addFields(
+          { name: `Início:`, value: `${vipsDBs[i].Inicio}` },
+          { name: `Fim:`, value: `${vipsDBs[i].Fim}` }
+        );
 
-                
-            } else {
-                console.log(`Verificação de VIP concluida. ${new Date()}`)
-            }
+        await removeVIP();
 
-        }
-        
+        logChannel.send({ content: `@here \n VIP REMOVIDO!`, embeds: [embed] });
 
-    }, 10000);
-
-
-
-
-
-
+      }
+      let data = new Date();
+      let dataFormatada = data.toLocaleString("pt-BR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        day: "2-digit",
+        month: "2-digit",
+        year: "2-digit",
+      });
+      console.log(`Verificação de VIP concluida. ${dataFormatada}`);
+    }
+  }, 30000);
 };
